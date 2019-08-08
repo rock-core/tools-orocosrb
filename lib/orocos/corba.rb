cf34848d839a49d81382d11579ac4340293c9ab5
lcdanter <leon_cedric.danter@dfki.de>
2019-08-08 17:18:02 +0200

    Implement a restart_name_server method to resolve rescued CORBA::ComError
    Change the error handling of > rescue ComError < from raise ComError to
    CORBA.warn and handle the error with a new method called
    "restart_name_server", which stops, cleanes and restarts the
    omniorb4-nameserver in /etc/init.d

--- lib/orocos/corba.rb
+++ lib/orocos/corba.rb
@@ -98,6 +98,23 @@ module Orocos
 	    result
 	end
 
+        # Stop and restart name server to resolve Orocos::CORBA::ComError
+        # 
+        # @ return [Boolean]
+        def restart_name_server?
+            system("sudo", "/etc/init.d/omniorb4-nameserver", "stop")
+            system("echo", "       Removing /var/lib/omniorb/*")
+            # needs to be a sub shell (exec) due to write-protection
+            # fork is needed to keep the script running after exec execution
+            exec("sudo rm -f /var/lib/omniorb/* && exit") if fork.nil?
+            system("sudo", "/etc/init.d/omniorb4-nameserver", "start")
+            puts "Successfully restarted omniorb4-nameserver"
+            return true
+          raise SystemCallError
+            puts "Restarting omniorb4-nameserver failed"
+            return false
+        end
+
         # Deinitializes the CORBA layer
         #
         # It shuts down the CORBA access and deregisters the Ruby process from
@@ -117,7 +134,13 @@ module Orocos
 
         rescue ComError => e
             if !obj1
-                raise ComError, "communication failed with #{obj0}", e.backtrace
+                CORBA.warn "Communication failed with #{obj0}"
+                CORBA.warn "{e.backtrace}\n"
+                puts "Restarting omniorb4 nameserver. Sudo required.."
+                unless restart_name_server?
+                  puts "Try to manually restart the omniorb4 nameserver at /etc/init.d/omniorb4-nameserver"
+                  exit
+                end
             else
                 raise ComError, "communication failed with either #{obj0} or #{obj1}", e.backtrace
             end
